29327fb13c53808c2860a6cf2399d10829faae40
Freek Driesenaar <freek.driesenaar@digital-me.nl>
2019-10-28 08:50:10 +0100

    Topic/server callbacks (#67)
    * Update openapi.json
    
    AccessFeedCallback response
    - set minItems to 0 for error member
    
    * Update openapi.json
    
    - changed title to 'Qiy Node API - General'
    - moved Server-related contents to ./Server/openapi.json
    
    * Create openapi.json
    
    Qiy Node API - Server
    - describes Server-related api
    Definitions
    - changed 'access feed callback' to 'services request'
    - changed 'feed request callback' to 'feed request'
    
    * Update Qiy Node API.md
    
    Definitions
    - renamed 'Feed Request Callback' to 'Server Feed Request'
    - renamed 'Access Feed Callback' to 'Services Request'
    Links
    - new: Server Feed Request request
    - new: Services Request request
    
    * Create json2yaml.py
    
    * Update openapi.json
    
    Example access feed callback response
    - deleted
    
    * Update Qiy Node API.md
    
    Links
    - fixes for links to server api
    
    * Update openapi.json
    
    Html anchors
    - updated
    Feed request response
    - fixed
    
    * Generated
    
    * Update openapi.json
    
    Reverse commit
    - set minItems for error in access response back to 1
    
    * Update openapi-doc.html
    
    - commit reverse
    Reset minItems of errors in access response.
    
    * Update openapi.json
    
    Feeds access response
    - fix: added missing error property
    
    * Regenerated
    
    * Fixes for mandatory error property
    
    * Removed Access Feed Callback

---
+++ Qiy-Node/json2yaml.py
@@ -0,0 +1,16 @@
+from json import load
+from yaml import dump
+
+def j2y(filename):
+    src=filename
+    dst=src.replace(".json",".yaml")
+    with open(src,'r') as f:
+        t=load(f)
+
+    with open(dst,'w') as f:
+        f.write(dump(t))
+
+
+j2y('openapi.json')
+j2y('Server/openapi.json')
+
